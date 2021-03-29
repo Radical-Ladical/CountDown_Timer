670e9c083131ab39b686235a56b35624ec27a1fa
Rad Lad <radlad@Ramiros-Air.home>
2021-03-29 09:08:16 -0500

    changing to react

---
+++ CountDown_Timer/countdown_timer/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <React.fragment>
+      <h1>
+        
+      </h1>
+    </React.fragment>
+  );
+}
+
+export default App;
